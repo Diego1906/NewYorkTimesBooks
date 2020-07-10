58666dd815b826c1b99b19f496df505199467b48
Diego <di_fsantos@yahoo.com.br>
2020-07-10 00:15:22 -0300

    Implementação dos componentes - Toolbar - RecyclerView

--- app/src/main/java/com/example/newyorktimesbooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/newyorktimesbooks/presentation/books/BooksActivity.kt
@@ -1,13 +1,17 @@
 package com.example.newyorktimesbooks.presentation.books
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.newyorktimesbooks.R
+import kotlinx.android.synthetic.main.toolbar.*
 
 class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toolbarMain.title = getString(R.string.title)
+        setSupportActionBar(toolbarMain)
     }
 }
