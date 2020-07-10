5e111055f19faee81ffe3b43616a664cd4a44fa6
Diego <di_fsantos@yahoo.com.br>
2020-07-09 23:42:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/newyorktimesbooks/presentation/books/BooksActivity.kt
@@ -0,0 +1,13 @@
+package com.example.newyorktimesbooks.presentation.books
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.newyorktimesbooks.R
+
+class BooksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_books)
+    }
+}
